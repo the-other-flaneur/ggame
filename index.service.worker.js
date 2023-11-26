63ed135beef2bc76eb71288da27a32092a0769cc
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2023-11-26 18:23:30 -0300

    build 26112023182355

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1701033438|28499492980";
+const CACHE_VERSION = "1701033737|28798554273";
 const CACHE_PREFIX = "Maizena Web Game-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
