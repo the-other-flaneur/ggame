adca6fad5cf4bc52a97eb866444d8600fae51eeb
Nicolas de la Cruz <delacruzbordajn@gmail.com>
2024-10-09 20:11:27 -0300

    build 09102024201135

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728515002|1372972135';
+const CACHE_VERSION = '1728515449|1819775503';
 /** @type {string} */
 const CACHE_PREFIX = 'Maizena Web Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
